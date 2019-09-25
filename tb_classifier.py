7025683731e793c60edd5902c56e2cebd74a831a
worawich <voravich1@hotmail.com>
2019-09-25 18:34:45 +0700

    -start read vcf with vcfpy library -goal for drug resistant and lineage classify

--- tb_classifier.py
+++ tb_classifier.py
@@ -1,7 +1,50 @@
 import json
-import vcf
+import vcfpy
 from pathlib import Path
 
-vcf_file_name = ""
+vcfFile_name = "/Users/worawich/Download_dataset/TB_platform_test/test_data/test_data_bgi/97_typing_snp_vep_custom_ann_hgvs_vcftest.vcf"
 
-vcf_reader = vcf.Reader(open)
+reader = vcfpy.Reader.from_path(vcfFile_name)
+
+#vcfpy.record.Record.INFO
+
+header = reader.header
+vep_header = header.get_info_field_info('CSQ')
+vep_header_description = vep_header.description
+vep_field_order = vep_header_description.split(': ')[1].split('|')
+hgvsc_idx = 0
+hgvsp_idx = 0
+count = 0
+
+###########
+## Loop check hgvs field index
+###########
+
+for vep_field in vep_field_order:
+    if(vep_field == 'HGVSc'):
+        hgvsc_idx = count
+    elif(vep_field == 'HGVSp'):
+        hgvsp_idx = count
+
+    count+=1
+    vcfpy.Record
+###########
+## Loop Filter and Collect only record that give HGVS field
+## (My understanding right now is "record that has HGVSc and HGVSp is variant record that occur on gene")
+###########
+
+for record in reader:
+
+    info = record.INFO
+    csq = info.get('CSQ')
+
+
+    for vep_field in csq:
+        vep_field_list = vep_field.split("|")
+
+        if(vep_field_list[hgvsc_idx] == "" and vep_field_list[hgvsp_idx] == ""):
+            continue
+
+
+
+    print("")
